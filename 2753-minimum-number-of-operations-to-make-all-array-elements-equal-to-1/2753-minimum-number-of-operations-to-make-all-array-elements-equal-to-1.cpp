2d223ec17ef1eb524b446fa1988c2cdbe3625cb3
Mohammed Abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-11-12 18:24:20 +0200

    2753-MINIMUM-NUMBER-OF-OPERATIONS-TO-MAKE-ALL-ARRAY-ELEMENTS-EQUAL-TO-1 Stats: Time: 0 ms (100%), Space: 31.6 MB (72.22%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int gcd(int a, int b) { return (b == 0 ? a : gcd(b, a % b)); }
-    int Spf[1000005];
-
-    void SPF() {
-        for (int i = 1; i < 1000005; i++)
-            Spf[i] = i;
-        for (long long i = 2; i < 1000005; i++) {
-            if (Spf[i] != i)
-                continue;
-            for (long long j = i + i; j < 1000005; j += i) {
-                if (Spf[j] != j)
-                    continue;
-                Spf[j] = i;
-                // SPF[j]=min(SPF[j],(int)i);
-            }
-        }
-    }
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         int gc = 0, cnt = 0;
@@ -28,19 +12,17 @@ public:
         if (gc != 1) {
             ans = -1;
         } else {
-            ans = n * 2;
             if (cnt)
                 return ans = n - cnt;
+            ans = n * 2;
             for (int i = 0; i < n; i++) {
                 gc = nums[i];
-                cnt = gc != 1;
                 for (int j = i - 1; j >= 0; j--) {
                     gc = gcd(gc, nums[j]);
-                    if (gc == 1)  {       
-                        ans = min(ans, i-j+n-1);
+                    if (gc == 1) {
+                        ans = min(ans, i - j + n - 1);
                         break;
                     }
-                    cnt++;
                 }
             }
         }
