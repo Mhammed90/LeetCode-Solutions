21268c525fc816f0334a1a5aa4b8f8d2c7356305
Mohammed Abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-11-17 18:51:57 +0200

    1548-CHECK-IF-ALL-1S-ARE-AT-LEAST-LENGTH-K-PLACES-AWAY Stats: Time: 0 ms (100%), Space: 61.4 MB (74.91%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        int f = 0, cnt = 0;
-        int i = 0;
-        while (i < nums.size() && !nums[i])
-            i++;
-        i++;
-        for (; i < nums.size(); i++) {
-            if (!nums[i])
-                cnt++;
-            else {
-                if (cnt < k) {
+        int lasPos = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                if (lasPos == -1)
+                    lasPos = i;
+                else if (i - lasPos-1 < k)
                     return 0;
-                }
-                cnt = 0;
+                lasPos = i;
             }
         }
         return 1;
