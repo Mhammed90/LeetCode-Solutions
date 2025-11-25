752ea0d0c901a1418b8b0170c7e34fde61c2e144
Mohammed Abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-11-25 17:43:40 +0200

    1064-SMALLEST-INTEGER-DIVISIBLE-BY-K Stats: Time: 0 ms (100%), Space: 7.8 MB (52.58%) - LeetHub

--- 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
-        vector<long long> pre;
         long long n = 1;
-        for (int i = 1; i < 50006; i++) {
+        for (int i = 1; i <=k; i++) {
             if (n % k == 0) {
                 return i;
             }
