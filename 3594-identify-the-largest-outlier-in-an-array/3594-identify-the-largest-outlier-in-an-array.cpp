a8f0719f9fb64ae8d9d58c09c03874be34288860
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-12-08 17:47:41 +0200

    3594-IDENTIFY-THE-LARGEST-OUTLIER-IN-AN-ARRAY Stats: Time: 0 ms (100%), Space: 180 MB (97.01%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.cpp
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int getLargestOutlier(vector<int>& nums) {
         int n = nums.size();
-        map<int, int> freq;
+        int add = 1000;
+        vector<int> freq(2 * add + 5);
         int tot = 0;
         for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
+            freq[nums[i] + add]++;
             tot += nums[i];
         }
-        int mx = -20000;
-        for (auto& i : freq) {
-            i.second--;
-            tot -= i.first;
-            for (auto j : freq) {
-                if (j.second) {
-                    int sum = tot - j.first;
-                    if (sum == j.first) {
-                        mx = max(mx, i.first);
+        int mx = -2000;
+        for (int i = 1000; i >= -1000; i--) {
+            if (freq[i + add] > 0) {
+                int rem = tot - i;
+                if (abs(rem) % 2 == 0) {
+                    int mid = rem / 2;
+                    int cur = mid + add;
+                    if (cur >= 0 && cur <= 2000 && freq[cur]) {
+                        if (mid == i) {
+                            if (freq[cur] > 1) {
+                                return i;
+                            }
+                        } else if (freq[cur]) {
+                            return i;
+                        }
                     }
                 }
             }
-            i.second++;
-            tot += i.first;
         }
 
         return mx;
