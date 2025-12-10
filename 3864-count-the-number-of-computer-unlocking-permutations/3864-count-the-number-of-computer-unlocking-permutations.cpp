204d8da80d2857110c0bc66d243e012a6063f644
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-12-10 08:17:19 +0200

    3864-COUNT-THE-NUMBER-OF-COMPUTER-UNLOCKING-PERMUTATIONS Stats: Time: 3 ms (43.5%), Space: 92.8 MB (47%) - LeetHub

--- 3864-count-the-number-of-computer-unlocking-permutations/3864-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/3864-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int countPermutations(vector<int>& nums) {
-        int st = nums[0];
+        int mn = nums[0];
         int n = nums.size(), mnCount = 0;
         for (int i = 0; i < n; i++) {
-            if (nums[i] <= st) {
+            if (nums[i] <= mn) {
                 mnCount++;
-                st = nums[i];
+                mn = nums[i];
             }
         }
-        if (st != nums[0] || mnCount > 1) {
+        if (mn != nums[0] || mnCount > 1) {
             return 0;
         }
         const int Mod = 1e9 + 7;
-        long long a = 1, b = 1;
+        long long f = 1;
         for (int i = 2; i < n; i++) {
-            b = (a * 1LL * i) % Mod;
-            a = b;
+            f = (f * 1LL * i) % Mod;
         }
 
-        return b;
+        return f;
     }
 };
