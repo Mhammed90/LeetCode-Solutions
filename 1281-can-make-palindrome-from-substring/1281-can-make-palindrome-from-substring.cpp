6f981ecbd2df89c52bdf08472aca85a79054472e
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2025-12-12 14:28:52 +0200

    1281-CAN-MAKE-PALINDROME-FROM-SUBSTRING Stats: Time: 54 ms (66.06%), Space: 107.6 MB (69.69%) - LeetHub

--- 1281-can-make-palindrome-from-substring/1281-can-make-palindrome-from-substring.cpp
+++ 1281-can-make-palindrome-from-substring/1281-can-make-palindrome-from-substring.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    int freq[100005][26];
-    vector<int> get(int idx) {
-        vector<int> ret(26, 0);
-        if (idx == -1)
-            return ret;
-        for (int i = 0; i < 26; i++) {
-            ret[i] = freq[idx][i];
-        }
-        return ret;
-    }
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
         int n = s.size();
+        vector<int> dp(n + 1);
         for (int i = 0; i < n; i++) {
-            freq[i][s[i] - 'a']++;
+            dp[i] = 1 << (s[i] - 'a');
             if (i)
-                for (int j = 0; j < 26; j++)
-                    freq[i][j] += freq[i - 1][j];
+                dp[i] ^= dp[i - 1];
         }
-
         vector<bool> res(queries.size(), 1);
-
         int idx = 0;
         for (auto i : queries) {
             int l = i[0], r = i[1], k = i[2];
-            vector<int> a = get(r);
-            vector<int> b = get(l - 1);
+            int val = dp[r] ^ (l ? dp[l - 1] : 0);
             int cnt = 0;
             for (int j = 0; j < 26; j++) {
-                a[j] = a[j] - b[j];
-                cnt += (a[j] & 1);
-            }
-            if ((r - l + 1) & 1) {
-                cnt--;
+                if ((val >> j) & 1)
+                    cnt++;
             }
             cnt >>= 1;
             if (cnt > k)
