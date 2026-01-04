eb30894d4e3a8492666613e08c65ad5b3d34415c
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2026-01-04 13:39:09 +0200

    1284-FOUR-DIVISORS Stats: Time: 723 ms (17.73%), Space: 24.9 MB (19.7%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.cpp
+++ 1284-four-divisors/1284-four-divisors.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
-    int SPF[1000005];
+    int Freq[1000005];
+    int SUM [1000005];
 
-    void spf() {
+    void pre() {
         int N = 1000001;
-        for (int i = 0; i < N; i++)
-            SPF[i] = i;
-        for (int i = 2; i < N; i++)
-            SPF[i] = i;
-        for (int i = 2; i < N; i++) {
-            if (SPF[i] != i)
-                continue;
-            for (int j = i + i; j < N; j += i) {
-                if (SPF[j] != j)
-                    continue;
-                SPF[j] = i;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j < N; j += i) {
+                Freq[j]++; 
+                SUM[j] += i;
             }
         }
     }
 
-    int sumFourDivisors(vector<int>& nums) {
-        spf();
+    int sumFourDivisors(vector<int> &nums) {
+        pre();
         int ans = 0;
-        for (int i : nums) {
-            int d = SPF[i];
-            if (1LL * d * d == i || SPF[i] == i)
-                continue;
-            if (1LL * d * d * d == i) {
-                ans += 1 + d + d * d + i;
-                continue;
-            }
-            int sum = i + 1;
-            sum += d;
-            i /= SPF[d];
-            if (SPF[i] == i) {
-                sum += i;
-                ans += sum;
-            }
+        for (int i: nums) {
+         if(Freq[i]==4)ans+=SUM[i];
         }
 
         return ans;
