9366179a57942283fd455b8b5e58005f1aff1a95
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2026-01-08 11:13:34 +0200

    1569-MAX-DOT-PRODUCT-OF-TWO-SUBSEQUENCES Stats: Time: 24 ms (48.8%), Space: 15.2 MB (90.4%) - LeetHub

--- 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int dp[505][505][2]; 
-    int solve(int i, int j,bool takeAtLeastOne, const vector<int>& nums1,
-              const vector<int>& nums2) {
-        if (i == nums1.size() || j == nums2.size()) { 
-            if(takeAtLeastOne)
-            return 0; 
+      static const int INF = 1e9;
+    int dp[505][505][2];
 
-            return -1e9;
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j][0] = -INF;
+                dp[i][j][1] = 0;
+            }
         }
 
-        int& ret = dp[i][j][takeAtLeastOne];
-        if (~ret)
-            return ret;
-        ret = -1e9;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                for (int take = 0; take <= 1; take++) {
 
-        ret = max({solve(i + 1, j,takeAtLeastOne, nums1, nums2), solve(i, j + 1,takeAtLeastOne, nums1, nums2),
-                   solve(i + 1, j + 1,1, nums1, nums2) + (nums1[i] * nums2[j])});
+                    int op1 = dp[i + 1][j][take];
+                    int op2 = dp[i][j + 1][take];
+                    int takeBoth = nums1[i] * nums2[j] + dp[i + 1][j + 1][1];
+                    dp[i][j][take] = max({op1, op2, takeBoth});
+                }
+            }
+        }
 
-        return ret;
-    }
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        return dp[0][0][0]; 
 
-        memset(dp, -1, sizeof(dp));
-
-        return solve(0, 0,0, nums1, nums2);
     }
 };
