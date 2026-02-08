066a433c00a5618033c7272acd7beb6092970a32
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2026-02-08 02:08:07 +0200

    0110-BALANCED-BINARY-TREE Stats: Time: 0 ms (100%), Space: 23.1 MB (54.19%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,21 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-int lenght(TreeNode* root){
-    if(root==NULL)
-    return 0;
-    int l=-1,r=-1;
-    l=lenght(root->left),r=lenght(root->right); 
-    if(l==-1||r==-1||abs(l-r)>1)return -1;
-  return max(l,r)+1;
-}
+    int dep(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int l = -1, r = -1;
+        l = dep(root->left), r = dep(root->right);
+        if (l == -1 || r == -1 || abs(l - r) > 1)
+            return -1;
+        return max(l, r) + 1;
+    }
     bool isBalanced(TreeNode* root) {
-     if(root==NULL)return 1;
-    return lenght(root)!=-1;
+        if (root == NULL)
+            return 1;
+        return dep(root) != -1;
     }
 };
