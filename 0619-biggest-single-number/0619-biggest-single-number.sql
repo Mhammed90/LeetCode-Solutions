993c30db4bac372bf0a1b43d9fd2370fa1a01806
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2026-04-22 23:09:53 +0200

    0619-BIGGEST-SINGLE-NUMBER Stats: Time: 315 ms (73.34%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+
+
+select MAX(num) as num
+from  MyNumbers 
+where num in
+(
+    select num 
+    from MyNumbers 
+    group by num 
+    having count(*)=1
+)
