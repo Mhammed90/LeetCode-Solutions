95051d01aad607065bc1979a45a17b4b0bbc73c9
abdalkreem <109801087+Mhammed90@users.noreply.github.com>
2026-05-08 15:15:46 +0300

    3933-MINIMUM-JUMPS-TO-REACH-END-VIA-PRIME-TELEPORTATION Stats: Time: 1210 ms (25.83%), Space: 425.9 MB (10.33%) - LeetHub

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/3933-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/3933-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    void buildSPF(vector<int> &SPF,int mx) {
+    vector<int> SPF;
+    int mx;
+    void buildSPF() {
         int N = mx;
         for (int i = 0; i < N; i++) {
             SPF[i] = i;
@@ -15,26 +17,30 @@ public:
         }
     }
 
-    set<int> getPrims(int n, vector<int> &SPF) {
-        set<int> s;
+    vector<int> getPrims(int n) {
+        vector<int> s;
         while (n > 1) {
-            s.insert(SPF[n]);
-            n /= SPF[n];
+            int d = SPF[n];
+            while (n % d == 0) {
+                n /= d;
+            }
+            s.push_back(d);
         }
         return s;
     }
 
-    int minJumps(vector<int> &nums) {
+    int minJumps(vector<int>& nums) {
         int n = nums.size();
-        int mx = *max_element(nums.begin(), nums.end());
-        vector<vector<int> > adj(mx + 1);
-        vector<bool> vis(n+5, false);
-        vector<int> SPF(mx + 5);
-        buildSPF(SPF, mx + 5);
+        mx = *max_element(nums.begin(), nums.end());
+        mx+=5;
+        vector<vector<int>> adj(mx + 1);
+        vector<bool> vis(n + 5, false);
+        SPF = vector<int>(mx);
+        buildSPF();
         for (int i = 0; i < n; i++) {
             mx = max(mx, nums[i]);
-            auto g = getPrims(nums[i], SPF);
-            for (int j: g) {
+            auto g = getPrims(nums[i]);
+            for (int j : g) {
                 adj[j].push_back(i);
             }
         }
@@ -57,7 +63,7 @@ public:
                 q.push(u - 1);
             }
             if (SPF[nums[u]] == nums[u]) {
-                for (int p: adj[nums[u]]) {
+                for (int p : adj[nums[u]]) {
                     if (!vis[p]) {
                         q.push(p);
                         dis[p] = min(dis[p], c + 1);
